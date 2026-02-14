4c5a7f185c1b79c90b91d92e918203a3f619ca7d
Tiki CCC <xiaoyuyuan1201@gmail.com>
2026-02-14 08:31:53 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,8 +17,15 @@ export default function Home() {
   });
 
   return (
-    <div className="min-h-screen bg-[#0f0a1f]">
-      <Navbar />
+    <div className="min-h-screen bg-gradient-to-br from-[#0f0a1f] via-[#1a0f2e] to-[#0f0a1f] relative">
+      {/* 背景光效 */}
+      <div className="absolute inset-0 overflow-hidden pointer-events-none">
+        <div className="absolute top-20 left-10 w-72 h-72 bg-purple-600/20 rounded-full blur-3xl opacity-60" />
+        <div className="absolute bottom-40 right-20 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl opacity-40" />
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-80 h-80 bg-blue-600/10 rounded-full blur-3xl opacity-30" />
+      </div>
+      <div className="relative z-10">
+        <Navbar />
       <HeroSection />
       <AboutSection />
       <WorksSection projects={projects} isLoading={isLoading} />
